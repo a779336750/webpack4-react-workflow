68bf5269096778c48945f3837e57c4a8bef7113c
huangjiaxuan <huangjiaxuan@kingsoft.com>
2018-11-26 14:36:01 +0800

    eslint: parserOptions配置项新增"legacyDecorators": true,用于es7装饰器的语法支持
    babelrc配置
    新增@babel/plugin-proposal-decorators插件，用于es7装饰器的转换
    新增插件@babel/plugin-proposal-object-rest-spread，用于解构赋值的转换

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-
-import dva from 'dva';
 import ReactDOM from 'react-dom';
 import App from './page/app';
-
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/page/main/index.js
+++ src/page/main/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import styles from './index.less';
-import myImg from './images/test2.png';
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
