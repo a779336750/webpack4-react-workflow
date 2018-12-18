11a8e4dbe3fe812ed29311ccc50f18130f60c605
huangjiaxuan <huangjiaxuan@kingsoft.com>
2018-12-18 10:37:45 +0800

    新增data-utils，用于某些数据的转换

--- src/page/app/index.js
+++ src/page/app/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Main from '../main';
 import { hot } from 'react-hot-loader';
 import { isTypeOf } from '../../utils/utils/type-utils';
+import {
+  objectToJson,
+  base64UrlEncode,
+  base64UrlDecode,
+  getUrlParams
+} from '../../utils/utils/data-utils';
+import { Base64 } from 'js-base64';
 class App extends React.Component {
   constructor() {
     super();
@@ -10,7 +17,9 @@ class App extends React.Component {
     };
   }
   componentDidMount() {
-    console.log(isTypeOf({ a: 1 }, 'Array'));
+    console.log(base64UrlEncode(location.href));
+    console.log(base64UrlDecode(base64UrlEncode(location.href)));
+    console.log(getUrlParams());
   }
   onClick() {
     this.setState({
---
+++ src/utils/utils/data-utils.js
@@ -0,0 +1,68 @@
+import * as type from './type-utils';
+import { Base64 } from 'js-base64';
+
+/**
+ * 对象转JSON
+ * @param input
+ * @return {string}
+ */
+export function objectToJson(input) {
+  if (type.isObject(input)) {
+    return JSON.stringify(input);
+  } else {
+    throw Error('type of input must be a object');
+  }
+}
+
+/**
+ * url Base64转码
+ * @param input
+ * @return {*}
+ */
+export function base64UrlEncode(input) {
+  return Base64.encodeURI(input);
+}
+
+/**
+ * url Base64解码
+ * @param input
+ * @return {*}
+ */
+export function base64UrlDecode(input) {
+  return Base64.decode(input);
+}
+
+/**
+ * 获取页面url查询字符串
+ * input 页面url（location.href）
+ * @param input
+ */
+export function getUrlParams(input) {
+  const href = input || location.href;
+  const paramsArr = href.split('?')[1].split('&');
+  const paramsObj = {};
+  paramsArr.map(item => {
+    paramsObj[item.split('=')[0]] = item.split('=')[1];
+  });
+  return paramsObj;
+}
+
+/**
+ * 剔除指定 props
+ * 来源: babel-runtime/helpers/objectWithoutProperties
+ * 用法: withoutProps(this.props, ['status'])
+ * @param obj
+ * @param keys
+ */
+export function withoutProps(obj, keys) {
+  const target = {};
+
+  for (let i in obj) {
+    // noinspection JSUnfilteredForInLoop
+    if (keys.indexOf(i) >= 0) continue;
+    if (!Object.prototype.hasOwnProperty.call(obj, i)) continue;
+    target[i] = obj[i];
+  }
+
+  return target;
+}
