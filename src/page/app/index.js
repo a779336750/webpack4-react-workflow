d840c9335c079fcdd569953a99afb6940bc0fa5b
huangjiaxuan <huangjiaxuan@kingsoft.com>
2018-12-19 10:11:18 +0800

    新增err-utils工具类库，用于抛出错误

--- src/page/app/index.js
+++ src/page/app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Base64 } from 'js-base64';
 import { isDev } from '../../utils/env.utils';
+import invariant from '../../utils/error-utils';
 import * as deviceUtils from '../../utils/device-utils';
 
 class App extends React.Component {
@@ -12,13 +13,7 @@ class App extends React.Component {
     };
   }
   componentDidMount() {
-    console.log(deviceUtils.isAndroid());
-    console.log(deviceUtils.isIOS());
-    console.log(deviceUtils.isMobile());
-    console.log(deviceUtils.isWeiXin());
-    console.log(deviceUtils.isIE());
-    console.log(process.env.NODE_ENV);
-    console.log(isDev());
+    invariant([], '不能为空字符串');
   }
   onClick() {
     this.setState({
---
+++ src/utils/error-utils.js
@@ -0,0 +1,9 @@
+import * as invariant from 'invariant';
+export default invariant;
+/**
+ *
+ *invariant(invariant,message)用法：
+ * 接受两个参数，
+ * invariant若为false,则会抛出提示为message的错误
+ * invariant若为true,则不会抛出错误
+ */
