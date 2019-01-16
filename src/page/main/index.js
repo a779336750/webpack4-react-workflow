54fe4e6b0713a24c2c6dd024b86b55d5d1aa0a67
huangjiaxuan <huangjiaxuan@kingsoft.com>
2019-01-16 17:10:18 +0800

    新增modal-dialog-hoc组件 使用高阶组件实现对业务组件进行封装，实现弹窗

--- src/page/main/index.js
+++ src/page/main/index.js
@@ -1,66 +1,22 @@
 import React from 'react';
 import styles from './index.less';
-import Modal from '@src/page/modal';
-import StarRating from '@src/components/star-rating';
-import Select from '@src/components/form/select';
+import ModalDialogHoc from '@src/components/modal-dialog-hoc';
 import { debounce } from '@src/utils/designing-mode-utils';
-export default class App extends React.Component {
-  componentDidMount() {
-    window.onscroll = debounce(function() {
-      console.log('sacassc');
-    }, 1000);
+@ModalDialogHoc
+class HelloWorld extends React.Component {
+  render() {
+    return <div>hello world,{this.props.name}</div>;
   }
+}
+export default class App extends React.Component {
+  showModal1 = () => {
+    HelloWorld.show();
+  };
   render() {
     return (
       <div className={'main'}>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <button onClick={this.showModal1}>showModal1</button>
+        <HelloWorld name={'dick'} />
       </div>
     );
   }
